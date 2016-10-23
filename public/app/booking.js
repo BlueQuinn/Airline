839298d6a5239b99f16dad45fc3aad4c0a04386f
DucThinh181 <h2152181@gmail.com>
2016-10-23 16:54:42 +0700

    FrontEnd
    add file booling,js

--- public/app/booking.js
+++ public/app/booking.js
@@ -2,7 +2,20 @@
  * Created by lequan on 10/20/2016.
  */
 
-app.controller('BookingController', function ($scope, Booking){
+var bookingModule = angular.module('app.booking', []);
+
+bookingModule.config(['$routeProvider', function ($routeProvider) {
+    'use strict';
+
+    $routeProvider.when('/booking', {
+        templateUrl: 'find.html',
+        controller: 'BookingController'
+    });
+}]);
+
+app.controller('BookingController', function ($scope, Booking, FlightService){
+
+    $scope.flights =FlightService.getFlights();
 
     $scope.reserve = function (booking) {
           Booking.save(booking, function () {
